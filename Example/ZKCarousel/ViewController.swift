8f24cfa032891a185a45ac1742402b5a2593138d
Zach <zachary.khan3@gmail.com>
2017-06-10 05:58:20 -0700

    removed emojis from title

--- Example/ZKCarousel/ViewController.swift
+++ Example/ZKCarousel/ViewController.swift
@@ -31,9 +31,9 @@ class ViewController: UIViewController {
     private func setupCarousel() {
         
         // Create as many slides as you'd like to show in the carousel
-        let slide = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo2"), title: "Hello There 👻", description: "Welcome to the ZKCarousel demo! Swipe left to view more slides!")
-        let slide1 = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo"), title: "A Demo Slide ☝🏼", description: "lorem ipsum devornum cora fusoa foen sdie ha odab ebakldf shjbesd ljkhf")
-        let slide2 = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo2"), title: "Another Demo Slide ✌🏼", description: "lorem ipsum devornum cora fusoa foen ebakldf shjbesd ljkhf")
+        let slide = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo2"), title: "Hello There", description: "Welcome to the ZKCarousel demo! Swipe left to view more slides!")
+        let slide1 = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo"), title: "A Demo Slide", description: "lorem ipsum devornum cora fusoa foen sdie ha odab ebakldf shjbesd ljkhf")
+        let slide2 = ZKCarouselSlide(image: #imageLiteral(resourceName: "demo2"), title: "Another Demo Slide", description: "lorem ipsum devornum cora fusoa foen ebakldf shjbesd ljkhf")
         
         // Add the slides to the carousel
         self.carousel.slides = [slide, slide1, slide2]
